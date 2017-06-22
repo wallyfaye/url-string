0c7aaec5901d313b6ed0484193981ad09660f6d6
Wally Faye <wally.faye@gmail.com>
2017-06-21 22:34:55 -0400

    loop thru string

--- UrlString.java
+++ UrlString.java
@@ -2,6 +2,17 @@ class UrlString{
 	public Boolean res = false;
 	public UrlString(String arg){
 		System.out.println(arg);
+		for(int i = 0; i < arg.length(); i++){
+			char letter = arg.charAt(i);
+			if(letter != " ".charAt(0)){
+				System.out.println(letter);
+			} else {
+				System.out.println("%");
+				System.out.println("2");
+				System.out.println("0");
+			}
+		}
+		System.out.println(arg);
 	}
 
 }
