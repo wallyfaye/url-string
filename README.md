fdb2836f2b69b6b22ff29cf13dafb03be4b04f4e
Wally Faye <wally.faye@gmail.com>
2017-06-22 22:12:56 -0400

    handle spaces

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Searches and replaces spaces in a string with `%20`
 ## Usage
 See contents of `run.sh` or use:
 >
-    sh run.sh the_first_string 
+    sh run.sh "  a demo   string  "
--- UrlString.java
+++ UrlString.java
@@ -1,12 +1,19 @@
 class UrlString{
 	public String res = "";
 	public UrlString(String arg){
+		boolean last_space = false;
 		for(int i = 0; i < arg.length(); i++){
 			char letter = arg.charAt(i);
 			if(letter != " ".charAt(0)){
+				if(last_space == true && this.res != ""){
+					this.res += "%20";
+				}
 				this.res += letter;
+				last_space = false;
 			} else {
-				this.res += "%20";
+				if(last_space == false){
+				}
+				last_space = true;
 			}
 		}
 	}
